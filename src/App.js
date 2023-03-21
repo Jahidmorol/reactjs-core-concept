3e2cdfc26817e2121f57b0eaf53a2842671ef6c5
jahid morol <jahidmorol2@gmail.com>
2023-03-21 20:40:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const ans = 'I am fine';
+const users = { name: "jahid hasan", id: 787898 };
+
+const userStyle = {
+  color: 'purple',
+  backgroundColor: 'red',
+  margin: '3rem',
+  padding: '3rem'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi friends how are you?</h1>
+      <h1>ans: {ans}</h1>
+
+      <div className="container">
+        <h1 style={userStyle}>Name: {users.name}</h1>
+        <h1 style={{background: 'purple',margin: '3rem',borderRadius: '1rem'}}>Id: {users.id}</h1>
+      </div>
+    </div>
+  );
+}
+
+function CreateName() {
+  return (
+    <div>
+      <h1>Name: {users.name}</h1>
+      <h1>Id: {users.id}</h1>
     </div>
   );
 }
